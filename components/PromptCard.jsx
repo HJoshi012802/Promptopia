1b90215ed4d0e55a800b9b5a17ef955d58b71afa
Harshit Joshi28 <harshitjoshi2002@gmail.com>
2023-06-16 00:56:10 +0530

    start commit

---
+++ components/PromptCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PromptCard =()=>{
+    return(
+        <div> Prompt Cards</div>
+    )
+}
+
+export default PromptCard;
---
+++ components/Provider.jsx
@@ -0,0 +1,9 @@
+import Reeact from 'react';
+
+ const Provider =()=>{
+    return(
+        <div> Provider </div>
+    )
+ }
+ 
+ export default Provider;
